3d61cafe49f334bbc45ac084ee84c6ba57f7a9cb
Tuty3107 <linhtuty3107@gmail.com>
2022-05-04 16:51:28 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -135,5 +135,21 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active2";
     setTimeout(showSlides, 5000);
 }
+/*--------btnToTop--------*/
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 
